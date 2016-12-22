d59e0474b6177c9ed5c0290137a9df4634352f96
Austin Blain <austin.a.blain@gmail.com>
2016-12-22 14:24:48 -0800

    add html, css, js to shrink info button when pressed

--- js/scripts.js
+++ js/scripts.js
@@ -13,18 +13,19 @@ $(document).ready(function() {
   $(".section-boxes .close-box").click(function() {
     // debugger;
     $(".not-yet").css('max-height', '60px');
-    $(".inside-box div").css('opacity', '0')
-    $("body div").removeClass("smallerr")
+    $(".inside-box div").css('opacity', '0');
+    $("body div").removeClass("smallerr");
     $(".sections div").removeClass("chosen");
-    // $(".inside-box").addClass("not-yet");
+    $(".inside-box .info-span").css('padding', '20px 40px');
     $(".inside-box").removeClass("background-black");
     event.stopPropagation();
   });
 
   $(".inside-box .info-span").click(function() {
     // $(".inside-box").removeClass("not-yet");
+    $(".inside-box .info-span").css('padding', '10px 20px');
     $(".not-yet").css('max-height', '600px');
-    $(".inside-box div").css('opacity', '1')
+    $(".inside-box div").css('opacity', '1');
     $(".inside-box").addClass("background-black");
     event.stopPropagation();
   });
